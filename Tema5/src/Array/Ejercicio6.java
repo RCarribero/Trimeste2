8459d1f3387996235fcabb4cebd069f73aa47991
RBX <rcarribero@proton.me>
2025-01-22 23:09:57 +0100

    .

--- Tema5/src/Array/Ejercicio6.java
+++ Tema5/src/Array/Ejercicio6.java
@@ -1,5 +1,27 @@
 package Array;
 
-public class Ejercicio6 {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Indica el tamaño de la matriz");
+        int n = input.nextInt();
+        int[][] matriz= new int[n][n];
+        rellenarArray(matriz, n);
+        Arrays.toString(matriz);
+        System.out.println(Arrays.deepToString(matriz));
+    }
+    public static int[][] rellenarArray(int[][] matriz, int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                Random random = new Random();
+                matriz[i][j]=random.nextInt(100);
+            }
+        }
+        return matriz;
+    }
 }
---
+++ Tema5/src/Array/Ejercicio8.java
@@ -0,0 +1,48 @@
+package Array;
+
+import java.util.Random;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        int[][] matrizA = new int[2][3];
+        int[][] matrizB = new int[3][2];
+        rellenarArray(matrizA);
+        rellenarArray(matrizB);
+        int[][] resultado = multiplicarMatriz(matrizA, matrizB);
+        mostrarMatriz(matrizA);
+        System.out.println();
+        mostrarMatriz(matrizB);
+        System.out.println();
+        mostrarMatriz(resultado);
+    }
+
+    public static void mostrarMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static int[][] rellenarArray(int[][] matriz) {
+    for(int i=0;i<matriz.length;i++){
+        for(int j=0;j<matriz[i].length;j++){
+            Random random = new Random();
+            matriz[i][j]=random.nextInt(10);
+        }
+    }
+    return matriz;
+    }
+    public static int[][] multiplicarMatriz(int[][]matrizA, int[][]matrizB) {
+        int[][] matrizResultado = new int[matrizA.length][matrizB[0].length];
+        for (int i = 0; i < matrizA.length; i++) {
+            for (int j = 0; j < matrizB[0].length; j++) {
+                matrizResultado[i][j] = 0;
+                for (int k = 0; k < matrizA[0].length; k++) {
+                    matrizResultado[i][j] += matrizA[i][k] * matrizB[k][j];
+                }
+            }
+        }
+        return matrizResultado;
+    }
+}
